f5757657ede3a406c02274ab9d5172b822986596
nathanbaum <nathan.baum@nyu.edu>
2018-05-31 03:12:44 -0400

    Completed Assignment 1: Flock Sim

---
+++ Assets/Assignments/Assignment_01/A01_nb2255/Scripts/Flock_Manager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nb2255
+{
+    public class Flock_Manager : MonoBehaviour
+    {
+
+        public GameObject master;
+        public GameObject slave;
+        public int flockSize;
+
+        List<GameObject> slaves;
+
+        // Use this for initialization
+        void Start()
+        {
+            /*
+             * Vector3 A = new Vector3(0, 0, 0);
+             * Vector3 B = new Vector3(0, 0, 1);
+             * Debug.Log("The distance between " + A + " and " + B + " is " + Vector3.Distance(A, B));
+            */
+            
+
+            slaves = new List<GameObject>();
+            slaves.Add(slave);
+            GameObject newSlave = slave;
+            for (int i=0; i<flockSize; i++)
+            {
+                newSlave = Instantiate(newSlave);
+                //Debug.Log("new slave's position before translate: " + newSlave.transform.position);
+                newSlave.transform.position += new Vector3(Random.value * 4 - 2 , Random.value * 4 - 2, Random.value * 4 - 2);
+                //Debug.Log("new slave's position after translate: " + newSlave.transform.position);
+                slaves.Add(newSlave);
+            }
+
+            for (int i = 0; i < slaves.Count; i++)
+            {
+                List<GameObject> tempList = new List<GameObject>();
+                for (int j = 0; j < slaves.Count; j++)
+                {
+                    if (j != i)
+                    {
+                        tempList.Add(slaves[j]);
+                    }
+                }
+
+                string debugList = "";
+                for (int k = 0; k < tempList.Count; k++)
+                {
+                    debugList += tempList[k].transform.position;
+                }
+                Debug.Log("This is what the temporary slave list looks like now: " + debugList);
+
+                slaves[i].SendMessage("setSlaves", tempList);
+                slaves[i].SendMessage("setMaster", master);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_01/A01_nb2255/Scripts/Flock_Slave.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nb2255
+{
+    public class Flock_Slave : MonoBehaviour
+    {
+
+        public float repulsion;
+        public float speed;
+
+        GameObject master;
+        List<GameObject> slaves;
+
+        void setSlaves( List<GameObject> slaves )
+        {
+            this.slaves = slaves;
+        }
+
+        void setMaster( GameObject master )
+        {
+            this.master = master;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+            //this gives us the vector from our slave to the master
+            Vector3 positionDelta = master.transform.position - transform.position;
+
+            //Debug.Log("positionDelta: " + positionDelta);
+
+            //for each slave in the flock, look at it's distance from me
+            for (int i=0; i<slaves.Count; i++)
+            {
+                Vector3 vecComp = Vector3.Project(positionDelta, slaves[i].transform.position);
+                float dist = Vector3.Distance(transform.position, slaves[i].transform.position);
+                //then subtract from my current delta, the vector component in the direction of that flock member, but we only want to substract a lot if we are close by (so we try to avoid getting close to other flock members)
+                positionDelta -= (vecComp*repulsion) / dist;
+            }
+
+            if (positionDelta.magnitude > 1) 
+            {
+                positionDelta.Normalize();
+            }
+
+            positionDelta *= speed;
+
+            transform.position += positionDelta;
+            */
+
+            Vector3 positionDelta = master.transform.position - transform.position;
+
+            if (positionDelta.magnitude > 1)
+            {
+                positionDelta.Normalize();
+            }
+
+            positionDelta *= speed*3/4;
+
+            transform.position += positionDelta * Time.deltaTime;
+
+            Vector3 repulse = new Vector3(0, 0, 0);
+            for (int i = 0; i < slaves.Count; i++)
+            {
+                Vector3 direct = slaves[i].transform.position - transform.position;
+                float dist = Vector3.Distance(transform.position, slaves[i].transform.position);
+                repulse += (-1 * direct) / dist;
+            }
+
+            if (repulse.magnitude > 1)
+            {
+                repulse.Normalize();
+            }
+
+            repulse *= repulsion*speed/4;
+
+            transform.position += repulse * Time.deltaTime;
+        }
+    }
+}
