8123c5af01fa76706a5f2db546a9b9e1c4f9c41d
Anthony Ramirez <ar4477@nyu.edu>
2018-06-11 02:33:20 -0400

    assignment 2

---
+++ Assets/Assignments/Assignment_02/A02_ar4477/Scripts/Hit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ar4477.A02 
+{
+    public class Hit : MonoBehaviour
+    {
+        // declare variables
+        public GameObject target;
+        Renderer r;
+
+        void Start()
+        {
+            // set target to the rim
+            target = GameObject.Find("Rim");
+            r = target.transform.GetComponent<Renderer>();
+        }
+
+        void Update()
+        {
+            
+        }
+
+        // code based on a Unity forum post
+        void OnCollisionEnter(Collision col)
+        {
+            // check to see if object collided with is the rim
+            if (col.gameObject.name == "Rim")
+            {
+                // change color upon contact and then destroy target
+                r.material.color = Color.red;
+                Destroy(target, 1.5f);
+            }
+        }
+
+    } 
+}
+
---
+++ Assets/Assignments/Assignment_02/A02_ar4477/Scripts/Throw.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ar4477.A02 
+{
+    public class Throw : MonoBehaviour
+    {
+        // decalre variables
+        public GameObject bballPrefab;
+        public Transform player;
+        public float ballspeed;
+        public float speedx;
+        public float speedy;
+
+		private void Start()
+		{
+            // set speeds
+            speedx = 10.0f;
+            speedy = 10.0f;
+            ballspeed = 15000f;
+		}
+
+		void Update()
+        {
+            // determine x and y based on position
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * speedx;
+            var y = Input.GetAxis("Vertical") * Time.deltaTime * speedy;
+
+
+            // throw ball if space bar is pressed
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Shoot();
+            }
+        }
+
+
+        void Shoot()
+        {
+            // instantiate a basketball 
+            GameObject bball = Instantiate(bballPrefab, player.position, player.rotation) as GameObject;
+
+            // put ball 28 units in the air 
+            bball.transform.Translate(0,28,0);
+
+            // move the ball forward
+            bball.GetComponent<Rigidbody>().AddForce(transform.forward * ballspeed);
+
+            // destroy the ball after 6 seconds
+            Destroy(bball, 6.0f);
+        }
+
+    }
+}
