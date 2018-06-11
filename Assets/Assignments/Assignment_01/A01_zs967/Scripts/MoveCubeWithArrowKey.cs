ea9b9142da7cf178b8b6e9fbf63533d25cfccf8c
SZhongheng <35933371+SZhongheng@users.noreply.github.com>
2018-06-11 00:35:18 -0400

    assignment 2 zs967 (#52)
    * nuked repository, cloned rep again
    
    * added missing scene
    
    * k
    
    * k
    
    * changes
    
    * ok

---
+++ Assets/Assignments/Assignment_01/A01_zs967/Scripts/MoveCubeWithArrowKey.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace A01zs967
+{
+    public class MoveCubeWithArrowKey : MonoBehaviour
+    {
+        public float speed;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0, vertical);
+            gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+
+        }
+    }
+}
