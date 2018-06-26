0a6ca3155f99302a61e20af8b28372256cc4db3d
nathanbaum <nathan.baum@nyu.edu>
2018-06-26 02:14:33 -0400

    added physicsBox

---
+++ Assets/Assignments/Assignment_03/A03_nb2255/Scripts/PickupMe.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nb2255
+{
+    /***
+     * PickupMe component allows user to select this object and 
+     * move it with their gaze
+     ******/
+    public class PickupMe : MonoBehaviour
+    {
+        public bool grabbed = false;  // have i been picked up, or not?
+        public float grabStrength = 10f;
+        Rigidbody myRb;
+        StrobeSelected strobe;
+        float dist;
+
+        // Use this for initialization
+        void Start()
+        {
+            myRb = GetComponent<Rigidbody>();
+            strobe = GetComponent<StrobeSelected>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (grabbed) {
+                //float dist = Vector3.Distance(transform.position, Camera.main.transform.position);
+                //Debug.Log("Distance: " + dist);
+                Vector3 goTo = Camera.main.transform.forward * dist + Camera.main.transform.position;
+                Vector3 delta = (goTo - transform.position);
+                Debug.Log("Delta: " + delta);
+                GetComponent<Rigidbody>().AddForce( delta*grabStrength );
+            }
+        }
+
+        /*
+         * PickupOrDrop
+         * Handle the event when the user clicks the button while 
+         * gaze is on this object.  Toggle grabbed state.
+         */
+        public void PickupOrDrop()
+        {
+            if (grabbed)
+            {  // now drop it
+                grabbed = false;
+                strobe.trigger = false;
+                GetComponent<Rigidbody>().useGravity = true;
+            }
+            else
+            {   // pick it up:
+                // make it move with gaze, keeping same distance from camera
+                dist = Vector3.Distance(transform.position, Camera.main.transform.position);
+                grabbed = true;
+                strobe.trigger = true;   // turn on color strobe so we know we have it
+                Debug.Log("turning off gravity");
+                GetComponent<Rigidbody>().useGravity = false;
+
+            }
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_03/A03_nb2255/Scripts/TriggerPickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nb2255
+{
+    public class TriggerPickup : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ptr = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+                if (Physics.Raycast(ptr, out hit))
+                {
+                    Debug.Log("hit a something within distance!");
+                    PickupMe pickScript = hit.collider.gameObject.GetComponent<PickupMe>();
+                    if (pickScript != null)
+                    {
+                        pickScript.PickupOrDrop();
+                    }
+                }
+
+            }
+        }
+    }
+}
