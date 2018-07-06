2707d06d85260b13d5cd70cbf14317c3c9942268
nathanbaum <nathan.baum@nyu.edu>
2018-07-06 16:37:03 -0400

    made collidable terrain, added flying camera

---
+++ Assets/Assignments/Assignment_04/A04_nb2255/Scripts/move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nb2255
+{
+    public class move : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if ( Input.GetMouseButton(0) )
+            {
+                Debug.Log("mouse button down!");
+                Rigidbody rb = GetComponent<Rigidbody>();
+                Vector3 direction = Camera.main.transform.rotation * Vector3.forward;
+                rb.AddForce(direction);
+            }
+        }
+    }
+}
